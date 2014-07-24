ae97f8c363c8bf2f7b155b311c1040f0f550f819
Kaneisha <minnieneisha@fullsail.edu>
2014-07-24 13:40:18 -0400

    Deleted Unnecessary code

--- js/main.js
+++ js/main.js
@@ -1,63 +1,14 @@
 (function(){
 
-// 	$(document).ready(function(){
-
-// 	// Set the interval to be 5 seconds
-// 	var t = setInterval(function(){
-// 		$(".carousel ul").animate({marginLeft:-800},1000,function(){
-// 			$(this).find("li:last").after($(this).find("li:first"));
-// 			$(this).css({marginLeft:0});
-// 		})
-// 	},5000);
-// });
-
-/*$(function() {
-  var currentIndex = 0,
-      itemCount    = $('.carousel > li').length;
-
-  // add the active class to the first item to hide all the others
-  $('.carousel > li:eq(' + currentIndex + ')').addClass('active');
-
-  $(document).ready(function(){
-    var $active  = $('.carousel > li.active'),
-        isNext   = $(this).hasClass('next');
-    currentIndex = (currentIndex + (isNext ? 1 : -1)) % itemCount;
-
-    // go back to the last item if we hit -1
-    if (currentIndex === -1) {
-      currentIndex = itemCount - 1;
-    }
-
-    var $next = $('.carousel > li:eq(' + currentIndex + ')');
-    $active.addClass(isNext ? 'next-out' : 'prev-out');
-    $next.addClass('active').addClass(isNext ? 'next-in' : 'prev-in');
-    setTimeout(function() {
-      $active.removeClass('active next-out prev-out');
-      $next.removeClass('next-in prev-in');
-    }, 500);
-    return false;
-  });
-});*/
-
     $(document).ready(function() {
 
     $(".demo").owlCarousel({
 
-    autoPlay: 3000, //Set AutoPlay to 3 seconds
+    autoPlay: 5000, //Set AutoPlay to 5 seconds
 
-    // $(this).owl.goTo(3);
-
-    items : 2,
+    items : 2, //Show 2 items per slide
     itemsDesktop : [1199,2],
     itemsDesktopSmall : [979,2]
-
-    // $(".next").click(function(){
-    //   owl.trigger('owl.next');
-    // })
-
-    // $(".prev").click(function(){
-    //   owl.trigger('owl.prev');
-    // })
 
     });
 
